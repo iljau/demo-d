419688849873880e1130ba799d09115aac07321f
Ilja Umov <ilja.umov@gmail.com>
2025-06-11 20:40:45 +0300

    initial commit

---
+++ _test/run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -x
+
+curl -s -X POST localhost:8080/birthdays -H 'Content-type:application/json' -d '{"birthDate": "2000-06-22", "name": "John McClane"}'
+echo
+curl -s -X POST localhost:8080/birthdays -H 'Content-type:application/json' -d '{"birthDate": "2002-06-22", "name": "Jim McClane"}'
+echo
+curl -s -X POST localhost:8080/birthdays -H 'Content-type:application/json' -d '{"birthDate": "2010-06-22", "name": "Tom McClane"}'
+echo
+curl -s localhost:8080/birthdays
+echo
+curl -s -X PUT localhost:8080/birthdays/3 -H 'Content-type:application/json' -d '{"birthDate": "2020-06-22", "name": "Tim McClane"}'
+echo
+curl -s 'localhost:8080/birthdays/3'
+echo
+curl -s -X DELETE 'localhost:8080/birthdays/3'
+echo
+curl -s 'localhost:8080/birthdays/3'
+echo
+curl -s 'localhost:8080/birthdays'
+echo
+curl -s -X DELETE 'localhost:8080/birthdays/1'
+echo
+curl -s -X DELETE 'localhost:8080/birthdays/2'
+echo
+curl -s 'localhost:8080/birthdays'
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,163 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.web.bind.annotation.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import javax.sql.DataSource;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface BirthdayRepository extends JpaRepository<Birthday, Long> {
+
+}
+
+@Entity
+class Birthday {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "YYYY-MM-dd")
+	public Date birthDate;
+	public String name;
+
+	public Birthday() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+
+class BirthdayNotFoundException extends RuntimeException {
+	BirthdayNotFoundException(Long id) {
+		super("Could not find birthday " + id);
+	}
+}
+
+
+@RestControllerAdvice
+class BirthdayNotFoundAdvice {
+	@ExceptionHandler(BirthdayNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	String birthdayNotFoundHandler(BirthdayNotFoundException ex) {
+		return ex.getMessage();
+	}
+}
+
+@Configuration
+@EnableJdbcRepositories
+class ApplicationConfig extends AbstractJdbcConfiguration {
+	@Bean
+	DataSource dataSource() {
+
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		return builder.setType(EmbeddedDatabaseType.HSQL).build();
+	}
+
+	@Bean
+	NamedParameterJdbcOperations namedParameterJdbcOperations(DataSource dataSource) {
+		return new NamedParameterJdbcTemplate(dataSource);
+	}
+
+	@Bean
+	TransactionManager transactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
+}
+
+// https://spring.io/guides/tutorials/rest
+@RestController
+class BirthdayController {
+	private final BirthdayRepository repository;
+
+	BirthdayController(BirthdayRepository repository) {
+		this.repository = repository;
+	}
+
+	@GetMapping("/birthdays")
+	List<Birthday> all() {
+		return repository.findAll();
+	}
+
+	@PostMapping("/birthdays")
+	Birthday newBirthday(@RequestBody Birthday newBirthday) {
+		return repository.save(newBirthday);
+	}
+
+	// Single item
+	@GetMapping("/birthdays/{id}")
+	Birthday one(@PathVariable Long id) {
+		return repository.findById(id)
+				.orElseThrow(() -> new BirthdayNotFoundException(id));
+	}
+
+	@PutMapping("/birthdays/{id}")
+	Birthday replaceBirthday(@RequestBody Birthday newBirthday, @PathVariable Long id) {
+
+		return repository.findById(id)
+				.map(birthday -> {
+					birthday.setBirthDate(newBirthday.getBirthDate());
+					birthday.setName(newBirthday.getName());
+					return repository.save(birthday);
+				})
+				.orElseGet(() -> {
+					return repository.save(newBirthday);
+				});
+	}
+
+	@DeleteMapping("/birthdays/{id}")
+	void deleteBirthday(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+
+}
+
+@SpringBootApplication
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
